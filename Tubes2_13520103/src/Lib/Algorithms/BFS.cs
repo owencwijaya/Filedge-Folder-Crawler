2618cea9fae1743bca96e61d6cf661c4687e5565
clumsyyyy <owencwijaya@gmail.com>
2022-03-24 19:17:15 +0700

    added docs, restructured folder

---
+++ Tubes2_13520103/src/Lib/Algorithms/BFS.cs
@@ -0,0 +1,79 @@
+﻿namespace DirectoryTraversal.Lib.Algorithms
+{
+    internal class BFS : TraversalAlgorithm, ITraversable
+    {
+        readonly Queue<string> Q = new();
+        readonly Dictionary<string, bool> VisitedNodes = new();
+        public BFS(int DrawDelay = 25)
+        {
+            this.DrawDelay = DrawDelay;
+        }
+        void TraverseBFS(string DirPath)
+        {
+            Q.Clear();
+            VisitedNodes.Clear();
+            VisitedNodes[DirPath] = true;
+            Q.Enqueue(DirPath);
+
+            while (Q.Count > 0)
+            {
+                string DeqNode = Q.Dequeue();
+                DirectoryInfo DirMain = new(DeqNode);
+
+                // Bangkitkan simpul ekspan
+                foreach (DirectoryInfo Directory in DirMain.EnumerateDirectories().Reverse())
+                {
+                    if (!VisitedNodes.ContainsKey(Directory.FullName))
+                    {
+                        OnDirectory?.Invoke(Directory);
+                        Thread.Sleep(DrawDelay);
+                    }
+                }
+                foreach (FileInfo File in DirMain.EnumerateFiles().Reverse())
+                {
+                    if (!VisitedNodes.ContainsKey(File.FullName))
+                    {
+                        OnFile?.Invoke(File);
+                        Thread.Sleep(DrawDelay);
+                    }
+                }
+
+                // Visit ke setiap file dan directory
+                foreach (FileInfo File in DirMain.EnumerateFiles())
+                {
+                    if (!VisitedNodes.ContainsKey(File.FullName))
+                    {
+                        OnVisitedFile?.Invoke(File);
+                        if (File.Name == FileName)
+                        {
+                            OnFound?.Invoke(File);
+                            if (!AllOccurences)
+                            {
+                                Q.Clear();
+                                VisitedNodes.Clear();
+                                return;
+
+                            }
+                        }
+                        Thread.Sleep(DrawDelay);
+                    }
+                }
+                foreach (DirectoryInfo Directory in DirMain.EnumerateDirectories())
+                {
+                    if (!VisitedNodes.ContainsKey(Directory.FullName))
+                    {
+                        Q.Enqueue(Directory.FullName);
+                        OnVisitedDirectory?.Invoke(Directory);
+                        Thread.Sleep(DrawDelay);
+                    }
+                }
+            }
+        }
+        public void Traverse(string DirPath, string FileName, bool AllOccurences)
+        {
+            this.FileName = FileName;
+            this.AllOccurences = AllOccurences;
+            TraverseBFS(DirPath);
+        }
+    }
+}
---
+++ Tubes2_13520103/src/Lib/DirectoryTraversal.cs
@@ -0,0 +1,79 @@
+﻿namespace DirectoryTraversal.Lib
+{
+    using Algorithms;
+
+    public enum Algorithm
+    {
+        DFS,
+        BFS
+    }
+    public class DirectoryTraversal
+    {
+        readonly BFS TraverseBFS = new();
+        readonly DFS TraverseDFS = new();
+
+        public Action<FileInfo>? OnVisitedFile
+        {
+            set
+            {
+                TraverseDFS.OnVisitedFile = value;
+                TraverseBFS.OnVisitedFile = value;
+            }
+        }
+        public Action<DirectoryInfo>? OnVisitedDirectory
+        {
+            set
+            {
+                TraverseDFS.OnVisitedDirectory = value;
+                TraverseBFS.OnVisitedDirectory = value;
+            }
+        }
+        public Action<FileInfo>? OnFile
+        {
+            set 
+            { 
+                TraverseDFS.OnFile = value; 
+                TraverseBFS.OnFile = value; 
+            }
+        }
+
+        public Action<DirectoryInfo>? OnDirectory
+        {
+            set 
+            { 
+                TraverseDFS.OnDirectory = value; 
+                TraverseBFS.OnDirectory = value; 
+            }
+
+        }
+
+        public Action<FileInfo>? OnFound
+        {
+            set 
+            { 
+                TraverseDFS.OnFound = value;
+                TraverseBFS.OnFound = value;
+            }
+        }
+        public int DrawDelay 
+        {
+            set
+            {
+                TraverseBFS.DrawDelay = value;
+                TraverseDFS.DrawDelay = value;
+            }
+        }
+        public void Traverse(string DirPath, string FileName, bool AllOccurances, Algorithm Algo)
+        {
+            if (Algo == Algorithm.BFS)
+            {
+                TraverseBFS.Traverse(DirPath, FileName, AllOccurances);
+            } 
+            else if(Algo == Algorithm.DFS)
+            {
+                TraverseDFS.Traverse(DirPath, FileName, AllOccurances);
+            }
+        }
+
+    }
+}
